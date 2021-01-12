0fceab14d3bf2af6e75e01b935bf8b4002b431e0
lorenperalta <stinpc@hotmail.com>
2021-01-12 11:12:29 -0500

    Cliente crud

---
+++ Models/Cliente.cs
@@ -0,0 +1,39 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mineralab.Models
+{
+    [Table("T_clientes")]    
+    public class Cliente
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int id { get; set; }
+        
+        [Required(ErrorMessage = "Por favor ingrese un nombre")]   
+        [Display(Name="Nombre: ")]
+        [Column("nombre")]
+        public string nombre { get; set; }
+
+        [Required(ErrorMessage = "Por favor ingrese un ruc")]   
+        [Display(Name="Ruc: ")]
+        [Column("ruc")]
+        public string ruc { get; set; }
+  
+        [Display(Name="Telefono: ")]
+        [Range(9, Int32.MaxValue)]
+        [Column("telefono")]
+        public int telefono { get; set; }
+  
+        [Display(Name="Email: ")]
+        [Column("email")]
+        [EmailAddress]
+        public string email { get; set; }
+
+        [Required(ErrorMessage = "Por favor ingrese una direccion")]   
+        [Display(Name="Direccion: ")]
+        [Column("direccion")]
+        public string direccion { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Mineralab
 Inicio del proyecto 3/12/2020
 # databse migrations
-dotnet ef migrations add CreateIdentitySchema --context Mineralab.Data.ApplicationDbContext -o "D:\git\Mineralab1.1\Mineralab\Data\Migrations"
+dotnet ef migrations add CreateIdentitySchema --context Mineralab.Data.ApplicationDbContext -o "`D:\git\Mineralab1.1\Mineralab\Data\Migrations"
+dotnet ef migrations add CreateIdentitySchema --context Mineralab.Data.ApplicationDbContext -o "C:\Users\PC_\Desktop\Mineralab\Data\Migrations"
 # update databse
 dotnet ef database update
